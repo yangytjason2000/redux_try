789c2724c2b067ac71b1ea1769bce72ddf4347e3
yangytjason2000 <yangyt.jason2015@outlook.com>
2024-04-29 16:27:08 -0700

    Added redux counter

--- src/App.js
+++ src/App.js
@@ -1,31 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-import {useDispatch, useSelector} from "react-redux";
-import { startRotate, stopRotate } from './reducers/rotateReducer';
+import Counter from './features/counter/Counter';
 function App() {
-  const { rotate } = useSelector((state)=>state.rotate);
-  const dispatch = useDispatch();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className={
-          "App-logo" + 
-          (rotate ? "":"-paused")
-          }  alt="logo" 
-          onClick={rotate ? ()=>{dispatch(stopRotate())} : 
-          ()=>{dispatch(startRotate())}}/>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Counter/>
     </div>
   );
 }
--- src/store.js
+++
@@ -1,5 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import rotateReducer from './reducers/rotateReducer'
-export default configureStore({
-  reducer: {rotate:rotateReducer}
-})
